b0c4e31d913843878097eaf3815edb95df5b71b0
codethoisinhvien <nguyentruonggiang28081999@gmail.com>
2020-05-12 22:51:08 +0700

    12312

--- src/serializers/table_group_serializer.py
+++ src/serializers/table_group_serializer.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 
-from src.models import TableGroup
-
+from src.models import TableGroup,Table
+from .table_serializer import TableSerializer
 
 class TableGroupSerializer(serializers.ModelSerializer):
+    tables = serializers.SerializerMethodField("get_list_table")
     class Meta:
         model = TableGroup
         fields = '__all__'
 
+    def get_list_food(self, obj):
+        # print(obj.id)
+        food = Table.objects.filter(table_group=obj.id)
+        food_serializer = TableSerializer(food, many=True)
+        return food_serializer.data
 
 class TableGroupUpdateSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
