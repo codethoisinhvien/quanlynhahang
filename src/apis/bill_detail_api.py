b000ade60cb7a6c2cf66a83b211523c4f70b0227
phongthien <nguyentruonggiangptdq@gmail.com>
2020-05-19 20:21:22 +0700

    fix craet bill

--- src/apis/bill_detail_api.py
+++ src/apis/bill_detail_api.py
@@ -6,7 +6,7 @@ from src.serializers.bill_detail_serializer import BillDetailSerializer
 
 class BillDetail(APIView):
     def post(self, request):
-        bill_setail_serializer = BillDetailSerializer(data=request.data)
+        bill_setail_serializer = BillDetailSerializer(data=request.data,many=True)
         if bill_setail_serializer.is_valid():
             bill_setail_serializer.save()
             return Response({"success": True, 'bill_detail': bill_setail_serializer.data})
